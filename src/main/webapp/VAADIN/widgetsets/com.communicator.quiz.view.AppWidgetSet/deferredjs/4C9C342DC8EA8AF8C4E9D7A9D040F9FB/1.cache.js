b7f3537652d50b2d002f477340f5d8b12853c894
morozov-yury <morozov.yury.txt@yandex.ru>
2014-05-29 18:19:28 +0300

    Deleted widgetsets from repo.

--- src/main/webapp/VAADIN/widgetsets/com.communicator.quiz.view.AppWidgetSet/deferredjs/4C9C342DC8EA8AF8C4E9D7A9D040F9FB/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_communicator_quiz_view_AppWidgetSet.runAsyncCallback1('WKb(2779,1,LSf);_.wc=function _ae(){fpc((!apc&&(apc=new hpc),apc),this.a.d)};MTf(Qk)(1);\n//@ sourceURL=1.js\n')
--- src/main/webapp/VAADIN/widgetsets/com.communicator.quiz.view.AppWidgetSet/deferredjs/B7253CCD916C59D49ACD5B7B65037A98/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_communicator_quiz_view_AppWidgetSet.runAsyncCallback1('XKb(2779,1,ERf);_.Bc=function ebe(){kpc((!fpc&&(fpc=new mpc),fpc),this.b.e)};ESf(Rk)(1);\n//@ sourceURL=1.js\n')
