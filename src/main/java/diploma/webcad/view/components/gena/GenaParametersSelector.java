786612a4999a61198e30cb5dec623c14e96cd20d
morozov-yury <morozov.yury.txt@yandex.ru>
2014-06-17 17:34:22 +0300

    Update icons.

--- src/main/java/diploma/webcad/view/components/gena/GenaParametersSelector.java
+++ src/main/java/diploma/webcad/view/components/gena/GenaParametersSelector.java
@@ -3,23 +3,13 @@ package diploma.webcad.view.components.gena;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.vaadin.data.Property.ValueChangeEvent;
-import com.vaadin.data.Property.ValueChangeListener;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.OptionGroup;
-import com.vaadin.ui.TextArea;
 import com.vaadin.ui.VerticalLayout;
 
 import diploma.webcad.core.init.SpringContext;
 import diploma.webcad.core.service.GenaService;
 import diploma.webcad.view.WebCadUI;
-import diploma.webcad.view.components.VerticalSeparator;
-import diploma.webcad.view.components.gena.GenaMMParamSelector.MooreMealy;
 
 public class GenaParametersSelector extends VerticalLayout {
 	
