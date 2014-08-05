221043d2eabc06fd18d3f14fa564336c746ad7dd
morozov-yury <morozov.yury.txt@yandex.ru>
2014-08-05 22:01:40 +0300

    Add generation table with simulations.

--- src/main/java/diploma/webcad/view/service/ViewFactory.java
+++ src/main/java/diploma/webcad/view/service/ViewFactory.java
@@ -1,13 +1,12 @@
 package diploma.webcad.view.service;
 
 import java.sql.Timestamp;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.tepi.filtertable.FilterTable;
-
-import scala.annotation.meta.setter;
 
 import com.vaadin.data.Container;
 import com.vaadin.data.Item;
@@ -22,13 +21,19 @@ import com.vaadin.ui.CustomTable.RowHeaderMode;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextArea;
 
+import diploma.webcad.core.model.simulation.BatchSimulation;
+import diploma.webcad.core.model.simulation.BatchSimulationStatus;
 import diploma.webcad.core.model.simulation.Device;
 import diploma.webcad.core.model.simulation.GenaLaunch;
 import diploma.webcad.core.model.simulation.GenaPlacement;
 import diploma.webcad.core.model.simulation.GenaResultStatus;
+import diploma.webcad.core.model.simulation.XilinxProject;
+import diploma.webcad.core.service.XilinxProjectStatus;
 
 @org.springframework.stereotype.Component
 @Scope("singleton")
+@SuppressWarnings("unchecked")
+@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
 public class ViewFactory {
 	
 	public Component wrapComponent (Component content) {
@@ -69,26 +74,41 @@ public class ViewFactory {
 	}
 	
 	public FilterTable getGenaLaunchesTable (List<GenaLaunch> genaLaunches) {
-		FilterTable filterTable = new FilterTable();
-        filterTable.setSizeFull();
-
-        filterTable.setFilterBarVisible(true);
-
-        filterTable.setSelectable(true);
-        filterTable.setImmediate(true);
-        filterTable.setMultiSelect(true);        
-        
-        filterTable.setPageLength(5);
-        filterTable.setRowHeaderMode(RowHeaderMode.INDEX);
-        filterTable.setColumnCollapsingAllowed(true);
-        filterTable.setColumnReorderingAllowed(true);
-        filterTable.setContainerDataSource(getLaunchesContainer(genaLaunches));
-        filterTable.setColumnCollapsed("state", true);
-        filterTable.setVisibleColumns("id", "placement", "genaParams", "creationDate", "status");
+		FilterTable filterTable = getTipicalFilterTable(getLaunchesContainer(genaLaunches));
         filterTable.setSortContainerPropertyId("creationDate");
-        filterTable.setSortAscending(false);
-
         return filterTable;
+	}
+	
+	public FilterTable getSimulationsTable(List<BatchSimulation> batchSimulations) {
+		FilterTable filterTable = getTipicalFilterTable(getSimulationsComtainer(batchSimulations));
+        filterTable.setSortContainerPropertyId("creationDate");
+		return filterTable;
+	}
+	
+	public Container getSimulationsComtainer (List<BatchSimulation> batchSimulations) {
+		IndexedContainer cont = new IndexedContainer();
+		
+		cont.addContainerProperty("id", Long.class, null);
+		cont.addContainerProperty("creationDate", Timestamp.class, null);
+		cont.addContainerProperty("projectsCount", Integer.class, null);
+		cont.addContainerProperty("devicesCount", Integer.class, null);
+		cont.addContainerProperty("status", BatchSimulationStatus.class, null);
+        
+        for (BatchSimulation batchSimulation : batchSimulations) {
+        	Long id = batchSimulation.getId();
+        	cont.addItem(id);
+        	cont.getContainerProperty(id, "id").setValue(
+        			batchSimulation.getId());      	
+        	cont.getContainerProperty(id, "creationDate").setValue(
+        			batchSimulation.getCreationDate());     	
+        	cont.getContainerProperty(id, "projectsCount").setValue(
+        			batchSimulation.getProjects().size());     	
+        	cont.getContainerProperty(id, "devicesCount").setValue(
+        			batchSimulation.getDevices().size());   	
+        	cont.getContainerProperty(id, "status").setValue(batchSimulation.getStatus());
+        }
+		
+		return cont;
 	}
 	
 	public Container getLaunchesContainer (List<GenaLaunch> genaLaunches) {
@@ -103,12 +123,13 @@ public class ViewFactory {
         cont.addContainerProperty("status", GenaResultStatus.class, null);
         
         for (GenaLaunch launch : genaLaunches) {
-        	cont.addItem(launch);
-        	cont.getContainerProperty(launch, "id").setValue(launch.getId());
-        	cont.getContainerProperty(launch, "placement").setValue(launch.getPlacement());
-        	cont.getContainerProperty(launch, "genaParams").setValue(launch.getParams());
-        	cont.getContainerProperty(launch, "creationDate").setValue(launch.getCreationDate());
-        	cont.getContainerProperty(launch, "status").setValue(launch.getStatus());
+        	Long id = launch.getId();
+        	cont.addItem(id);
+        	cont.getContainerProperty(id, "id").setValue(id);
+        	cont.getContainerProperty(id, "placement").setValue(launch.getPlacement());
+        	cont.getContainerProperty(id, "genaParams").setValue(launch.getParams());
+        	cont.getContainerProperty(id, "creationDate").setValue(launch.getCreationDate());
+        	cont.getContainerProperty(id, "status").setValue(launch.getStatus());
         }
 		
 		return cont;
@@ -133,6 +154,50 @@ public class ViewFactory {
 		}
 		
 		return container;
+	}
+	
+	public FilterTable getProjectsTable (List<XilinxProject> projects) {
+		FilterTable table = getTipicalFilterTable(getXilinxProjectsContainer(projects));
+		table.setSortContainerPropertyId("family");
+		return table;
+	}
+	
+	public Container getXilinxProjectsContainer (List<XilinxProject> projects) {
+		IndexedContainer cont = new IndexedContainer();
+		
+		cont.addContainerProperty("id", Long.class, null);
+		cont.addContainerProperty("family", String.class, null);
+		cont.addContainerProperty("device", String.class, null);
+		cont.addContainerProperty("status", XilinxProjectStatus.class, null);
+		//cont.addContainerProperty("folderId", String.class, "");
+		
+		for (XilinxProject project : projects) {
+			Long id = project.getId();
+        	cont.addItem(id);
+        	cont.getContainerProperty(id, "id").setValue(id);
+        	cont.getContainerProperty(id, "family").setValue(
+        			project.getDevice().getDeviceFamily().getName());
+        	cont.getContainerProperty(id, "device").setValue(project.getDevice().getName());
+        	cont.getContainerProperty(id, "status").setValue(project.getStatus());
+        	//cont.getContainerProperty(id, "folderId").setValue(project.getFolder());
+		}
+		
+		return cont;
+	}
+	
+	private FilterTable getTipicalFilterTable (Container container) {
+		FilterTable filterTable = new FilterTable();
+		filterTable.setSizeFull();
+        filterTable.setFilterBarVisible(true);
+        filterTable.setSelectable(true);
+        filterTable.setImmediate(true);
+        filterTable.setMultiSelect(false); 
+        filterTable.setRowHeaderMode(RowHeaderMode.INDEX);
+        filterTable.setColumnCollapsingAllowed(true);
+        filterTable.setColumnReorderingAllowed(true);
+        filterTable.setContainerDataSource(container);
+        filterTable.setSortAscending(false);
+		return filterTable;
 	}
 
 }
